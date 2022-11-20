f15e85f44ea41c8976501be346dc62b8ee5cd229
MiriamMCh <103612596+MiriamMamaniChullo@users.noreply.github.com>
2022-11-20 17:05:53 -0500

    subir

---
+++ helpmefastfronted/src/app/app-routing.module.ts
@@ -0,0 +1,58 @@
+import { DistritoCreaeditaComponent } from './page/distrito/distrito-creaedita/distrito-creaedita.component';
+import { DistritoComponent } from './page/distrito/distrito.component';
+import { ComentariosCreaeditaComponent } from './page/comentarios/comentarios-creaedita/comentarios-creaedita.component';
+import { ComentariosComponent } from './page/comentarios/comentarios.component';
+import { EnfermeroCreaeditaComponent } from './page/enfermero/enfermero-creaedita/enfermero-creaedita.component';
+import { EnfermeroComponent } from './page/enfermero/enfermero.component';
+import { InteresCreaeditaComponent } from './page/interes/interes-creaedita/interes-creaedita.component';
+import { InteresComponent } from './page/interes/interes.component';
+import { AdultoJovenInsertarComponent } from './page/adulto-joven/adulto-joven-insertar/adulto-joven-insertar.component';
+import { AdultoJovenComponent } from './page/adulto-joven/adulto-joven.component';
+import { AnuncioCreaeditaComponent } from './page/anuncio/anuncio-creaedita/anuncio-creaedita.component';
+import { AnuncioComponent } from './page/anuncio/anuncio.component';
+import { ServiciosComponent } from './page/servicios/servicios.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ServiciosCreaeditaComponent} from './page/servicios/servicios-creaedita/servicios-creaedita.component';
+
+const routes: Routes = [
+  {path:'servicios',component:ServiciosComponent, children: [
+    { path: 'nuevo', component: ServiciosCreaeditaComponent },
+    { path: 'edicion/:id', component: ServiciosCreaeditaComponent }
+  ]},{
+    path:'anuncios',component: AnuncioComponent, children:[
+      {path:'nuevo', component: AnuncioCreaeditaComponent},
+      {path:'edicion/:id', component: AnuncioCreaeditaComponent}
+    ]
+  },{
+  path: 'adultoJoven', component: AdultoJovenComponent, children: [
+    { path: 'insertar', component: AdultoJovenInsertarComponent },
+    { path: 'edicion/:id', component: AdultoJovenInsertarComponent }
+   ]},{
+      path: 'intereses', component: InteresComponent, children: [
+        { path: 'nuevo', component: InteresCreaeditaComponent },
+        { path: 'edicion/:id', component: InteresCreaeditaComponent }
+    ]},
+    {
+      path: 'enfermeros', component: EnfermeroComponent, children: [
+        { path: 'nuevo', component: EnfermeroCreaeditaComponent },
+        { path: 'edicion/:id', component: EnfermeroCreaeditaComponent }
+    ]},
+    {
+      path: 'comentarios', component: ComentariosComponent, children: [
+        { path: 'nuevo', component: ComentariosCreaeditaComponent },
+        { path: 'edicion/:id', component: ComentariosCreaeditaComponent }
+    ]},
+    {
+      path: 'distrito', component: DistritoComponent, children: [
+        { path: 'nuevo', component: DistritoCreaeditaComponent },
+        { path: 'edicion/:id', component: DistritoCreaeditaComponent }
+    ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
---
+++ helpmefastfronted/src/app/page/comentarios/comentarios-creaedita/comentarios-creaedita.component.ts
@@ -0,0 +1,70 @@
+import { ComentariosService } from 'src/app/service/comentarios.service';
+import { Comentarios } from './../../../model/comentarios';
+import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute, Params} from '@angular/router';
+import { Servicios } from 'src/app/model/servicios';
+import { ServiciosService } from 'src/app/service/servicios.service';
+
+@Component({
+  selector: 'app-comentarios-creaedita',
+  templateUrl: './comentarios-creaedita.component.html',
+  styleUrls: ['./comentarios-creaedita.component.css']
+})
+export class ComentariosCreaeditaComponent implements OnInit {
+  comentario: Comentarios = new Comentarios();
+  mensaje: string = "";
+  edicion: boolean = false;
+  id: number = 0;
+
+  listaServicios: Servicios[]=[];
+  idServicioSeleccionado: number=0;
+
+  constructor(private comentarioService: ComentariosService, private router: Router, private route: ActivatedRoute, private serviciosService: ServiciosService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.init();
+    });
+    this.serviciosService.listar().subscribe(data => { this.listaServicios = data });
+  }
+  aceptar():void{
+    if(this.comentario.opinion.length > 0 && this.comentario.numCalificacion.length > 0 && this.idServicioSeleccionado>0){
+    let se = new Servicios();
+    se.idServicios=this.idServicioSeleccionado;
+    this.comentario.servicio=se;
+
+    if(this.edicion)
+    {
+      this.comentarioService.modificar(this.comentario).subscribe(data => {
+        this.comentarioService.listar().subscribe(data => {
+          this.comentarioService.setLista(data);
+        })
+      })
+    } else {
+    this.comentarioService.insertar(this.comentario).subscribe(data => {
+      this.comentarioService.listar().subscribe(data => {
+        this.comentarioService.setLista(data);
+      });
+    }, err => {
+      console.log(err);
+    });
+  }
+    this.router.navigate(['comentarios']);
+    } else {
+    this.mensaje = "Complete los valores requeridos";
+    }
+  }
+
+  init() {
+    if (this.edicion) {
+      this.comentarioService.listarId(this.comentario.idComentarios).subscribe(data => {
+        this.comentario = data;
+        console.log(data);
+        this.idServicioSeleccionado=data.servicio.idServicios;
+      });
+    }
+  }
+
+}
